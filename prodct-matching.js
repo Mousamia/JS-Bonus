a180d6160bfccdb7721d7e41fe36299ab8c51416
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-10 01:18:57 +0600

    product-match done

---
+++ prodct-matching.js
@@ -0,0 +1,25 @@
+
+const products = [
+    {id: 1, name: "smart-phone", price:10000},
+    {id: 2, name: "smart phone", price:21845},
+    {id: 3, name: "Huwaei Y prime", price:19000},
+    {id: 4, name: "I phone 14max", price:120000},
+    {id: 5, name: "appple xo", price:10000},
+    {id: 6, name: "Nokia Phone", price:100},
+    {id: 7, name: "Motorolla", price:1000},
+    {id: 8, name: "Green war", price:30000},
+    {id: 9, name: "HTC", price:20000},
+]
+
+function matchedItem(products, search){
+    let matched = [];
+    for(let product of products){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matched.push(product);
+        }
+    }
+    return matched;
+}
+
+const result = matchedItem(products, "phone");
+console.log(result);
