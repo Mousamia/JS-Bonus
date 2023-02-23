2de7bf7fc912756dcc66a56c35905b1d7f67c8b9
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-02-23 15:10:46 +0600

    practise reduce

--- map.js
+++ map.js
@@ -8,17 +8,32 @@ for(const number of numbers){
 // arrow function
 // const getMultiplied = number => number*3;
 
-for(const number of numbers){
-    const multiply = getMultiplied(number);
-    multiplied.push(multiply);
-}
+// for(const number of numbers){
+//     const multiply = getMultiplied(number);
+//     multiplied.push(multiply);
+// }
 
 
 const getDone = ["samia" , "lamia" , "samiha"].map(x => x + "chowdhury");
 console.log(getDone);
+const firstLetters = ["samia", "othoi", "nidhi", "kobita"].map(x => x[1]);
+console.log(firstLetters);
+
+const obj = {
+    name: "samia",
+}
+
+obj.age = 29;
+
+console.log(obj);
+
+const quiz = [12,5,7];
+console.log(Math.min(quiz));
 
 
-
+const nums = [1,2,3,4,5];
+let output = nums.filter(n => n%2 == false);
+console.log(output);
 
 
 
